4a18955d18a7c4e52b63cf4a04858be9cf9c1cd2
Debora Gama <debora.gama@instruct.com.br>
2022-11-22 22:54:49 -0300

    feat(recipes): creating contact and about views

--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from recipes.views import home
+from recipes.views import home, contact, about
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home),
+    path("contato/", contact),
+    path("sobre/", about),
+
 ]
 
--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('HOME')
+
+
+def contact(request):
+    return HttpResponse('CONTATO')
+
+
+def about(request):
+    return HttpResponse('SOBRE')
