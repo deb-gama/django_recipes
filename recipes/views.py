1b1b755b2198caeece1220d65b18a1334bc28588
Debora Gama <debora.gama@instruct.com.br>
2022-11-27 21:25:00 -0300

    feat(recipes): creating db data and render the data in the home view

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from recipes.utils.factory import make_recipe
+from recipes.models import Recipe
 
 def home(request):
     title = 'Home | Recipes'
-    return render(request, 'recipes/pages/home.html', {'title': title, 'recipes': [make_recipe() for _ in range(10) ] })
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', {'title': title, 'recipes': recipes })
 
 def recipe(request, recipe_id):
     title = 'Details | Recipes'
