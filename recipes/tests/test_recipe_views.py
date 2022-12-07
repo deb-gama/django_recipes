5d2d9401637e541459c3fc9636ba18b3a451e0d6
Debora Gama <debora.gama@instruct.com.br>
2022-12-06 23:24:20 -0300

    feat(recipes): creating tests for home view

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -17,6 +17,9 @@ class RecipeViewsTest(TestCase):
                                   kwargs={'recipe_id': self.recipe_id}
                                   )
         self.home_url = reverse('recipes:home')
+        self.response_home = self.client.get(self.home_url)
+        self.response_category = self.client.get(self.category_url)
+        self.response_recipe = self.client.get(self.recipe_url)
 
     def test_recipe_home_view_function(self):
         """
@@ -39,3 +42,22 @@ class RecipeViewsTest(TestCase):
         """
         view = resolve(self.recipe_url)
         self.assertIs(view.func, views.recipe)
+
+    def test_home_view_return_status_code_200(self):
+        """
+        Tests if home url returns a 'ok' status code
+        """
+        self.assertEqual(self.response_home.status_code, 200)
+
+    def test_home_view_render_correct_template(self):
+        """
+        Tests if home url render the correct template
+        """
+        self.assertTemplateUsed(self.response_home, 'recipes/pages/home.html')
+
+    def test_recipe_home_view_with_no_recipes_found(self):
+        """
+        Tests if no recipes message appears in home page when there are not recipes to show
+        """
+        html_to_string = self.response_home.content.decode('utf-8')
+        self.assertIn('No recipes here', html_to_string)
