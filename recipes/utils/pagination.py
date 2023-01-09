5c60a78ed5731e6e800b58a20b706ef8d79b064c
Debora Gama <gama.debora1@gmail.com>
2023-01-09 08:46:45 -0300

    feat(recipes): adding pycache files to gitignore

--- recipes/utils/pagination.py
+++ recipes/utils/pagination.py
@@ -1,6 +1,21 @@
+import math
+
+
 def make_pagination_range(
     page_range,
     qt_pages,
     current_page,
 ):
-    return [1, 2, 3, 4]
+    middle_range = math.ceil(qt_pages/2)
+    # vai arredondar o meio do range
+    start_range = current_page - middle_range
+    stop_range = current_page + middle_range
+
+    # tratando o range offset - saiu fora do range negativo -  abs pra transformar de neg pra pos
+    start_range_offset = abs(start_range) if start_range < 0 else 0
+
+    if start_range < 0:
+        start_range = 0
+        start_range += start_range_offset
+
+    return page_range[start_range:stop_range]
--- recipes/utils/test_pagination.py
+++ recipes/utils/test_pagination.py
@@ -13,12 +13,27 @@ class PaginationTest(TestCase):
 
         self.assertEqual([1, 2, 3, 4], pagination)
 
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa: E501
 
-def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa: E501
-    pagination = make_pagination_range(
-        page_range=list(range(1, 21)),
-        qt_pages=4,
-        current_page=1,
-    )
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qt_pages=4,
+            current_page=1,
+        )
 
-    self.assertEqual([1, 2, 3, 4], pagination)
+        self.assertEqual([1, 2, 3, 4], pagination)
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qt_pages=4,
+            current_page=2,
+        )
+
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qt_pages=4,
+            current_page=3,
+        )
+
+        self.assertEqual([2, 3, 4, 5], pagination)
