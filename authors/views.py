1a658b94bd2c9e854afe7b5b924f0eb6acfaa22d
Debora Gama <debora.gama@instruct.com.br>
2023-02-05 22:49:22 -0300

    feat(recipes): creating a redirection for pot method form

--- authors/views.py
+++ authors/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 
 from .forms import RegisterForm
 
 
 def register_view(request):
-    request.session['number'] = request.session.get('number') or 1
-    # somando aqui para desconsiderar o NONE que recebemos no primeiro acesso
-    request.session['number'] += 1
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     title = 'Auhors | Register'
     return render(request, 'authors/pages/register_view.html', {'form': form, 'title':title})
 
 def register_create(request):
     if  not request.POST:
         raise Http404()
-    form = RegisterForm(request.POST)
 
-    title = 'Auhors | Register'
-    return render(request, 'authors/pages/register_view.html', {'form': form, 'title':title})
+    POST = request.POST
+    request.session['register_fomr_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
 
 
