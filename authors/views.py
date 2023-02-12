2192ec7b5f1676d133a69684f9e9e3e54e8a7c6d
Debora Gama <debora.gama@instruct.com.br>
2023-02-12 13:12:39 -0300

    doc(recipes): docstring in authors views

--- authors/views.py
+++ authors/views.py
@@ -6,6 +6,10 @@ from .forms import RegisterForm
 
 
 def register_view(request):
+    """
+    This view is responsible for rendering an empty form when in GET mode and
+    receiving the register_form_data in the register redirection create view.
+    """
     register_form_data = request.session.get('register_form_data', None)
     title = 'Auhors | Register'
     form = RegisterForm(register_form_data)
@@ -20,6 +24,10 @@ def register_view(request):
     )
 
 def register_create(request):
+    """
+    This view will log the new user or throw an error if something is wrong.
+    Redirect to register_view to disallow resubmit of the form.
+    """
     if  not request.POST:
         raise Http404()
 
