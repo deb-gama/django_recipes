0b9b3ac258acb32d284aa07aa5c922b50b696728
Debora Gama <debora.gama@instruct.com.br>
2023-04-02 18:06:06 -0300

    feat(recipes_backend): creating a serializer for Recipe model

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from models import Recipe
 
 @api_view()
 def recipe_api_list(request):
