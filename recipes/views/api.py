24054a77db25cbc9ac974f15496bddd2b0edc63a
Debora Gama <debora.gama@instruct.com.br>
2023-04-02 18:27:06 -0300

    feat(recipes_backend): adding a mangaer to Recipe Model

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -2,7 +2,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from models import Recipe
+from recipes.models import Recipe
 
 @api_view()
 def recipe_api_list(request):
