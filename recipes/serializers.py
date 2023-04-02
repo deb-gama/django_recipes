c124eb24323c6453f3541d44a0a14c3995f62374
Debora Gama <debora.gama@instruct.com.br>
2023-04-02 20:12:20 -0300

    feat(recipes_backend): adding author id on json response

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
-from recipes.models import Category
+from django.contrib.auth.models import User
 
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
     category_name = serializers.StringRelatedField(source='category')
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
