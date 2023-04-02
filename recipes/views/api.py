f80bc7006223a0a6c4f59d088efc14ae3d2f0dff
Debora Gama <debora.gama@instruct.com.br>
2023-04-02 18:30:31 -0300

    feat(recipes_backend): adding serializer on recipe api list view

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,7 +3,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from recipes.models import Recipe
+from recipes.serializers import RecipeSerializer
 
 @api_view()
 def recipe_api_list(request):
-    return Response('ok')
+    recipes = Recipe.objects.get_published()[:10]
+    serializer = RecipeSerializer(instance=recipes, many=True)
+
+    return Response(serializer.data)
