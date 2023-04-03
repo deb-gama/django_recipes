f3292ae7f024d387ad6fc3d8986a713faa36cbe3
Debora Gama <debora.gama@instruct.com.br>
2023-04-02 22:57:53 -0300

    feat(recipes_backend): adding validation

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from recipes.models import Recipe
@@ -12,4 +13,27 @@ class RecipeSerializer(serializers.ModelSerializer):
         source='is_published',
         read_only=True,
     )
+
+    def validate(self, attrs):
+        super_validate = super().validate(attrs)
+        title = attrs.get('title')
+        description = attrs.get('description')
+
+        if title == description:
+            raise serializers.ValidationError(
+                {
+                "title": ["Cannot be equal to description", "Please give another title"],
+                "description": ["Cannot be equal to title", "Please try another description"]
+                }
+            )
+
+        return super_validate
+
+
+    def validate_title(self, value):
+        title = value
+
+        if len(title) < 5:
+            raise serializers.ValidationError('Must have at least 5 chars.')
+        return title
 
