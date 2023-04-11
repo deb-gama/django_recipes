1bdebbeffc94ab740c1aac2d19014af098840f72
Debora Gama <debora.gama@instruct.com.br>
2023-04-10 21:19:57 -0300

    refactor(recipes_backend): refactoring api views using viewsets

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,8 +3,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, ListAPIView
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 from django.shortcuts import get_object_or_404
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
@@ -13,50 +14,26 @@ from recipes.serializers import RecipeSerializer
 class RecipeAPIv1Pagination(PageNumberPagination):
     page_size = 3
 
+
+class RecipeAPIv1ViewSet(ModelViewSet):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv1Pagination
+
 class RecipesAPIv1List(ListCreateAPIView):
     """
-    View that contains the get anmd post methods and have the same url without a pk in it.
+    View that contains the get and post methods and have the same url without a pk in it.
     The 'get' method list recipes and the 'post' method create a new recipe.
     """
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv1Pagination
 
-class RecipeAPIv1Detail(APIView):
-    def get_recipe(self, pk):
-        recipe = get_object_or_404(Recipe.objects.get_published(), pk=pk)
-
-        return recipe
-
-    def get(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            many=False,
-            context={'request':request},
-        )
-        return Response(serializer.data)
-
-    def patch(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            data = request.data,
-            many=False,
-            context={'request':request},
-            partial = True,
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(
-            serializer.data,
-        )
-
-    def delete(self, request, pk):
-        recipe = self.get_recipe(pk)
-        recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
+class RecipeAPIv1Detail(RetrieveUpdateDestroyAPIView):
+    """
+    View that contains the read, patch and delete methods and have the same url with a pk in it.
+    The 'get' method list recipes and the 'post' method create a new recipe.
+    """
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv1Pagination
