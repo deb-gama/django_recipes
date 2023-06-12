5c1ce9feafb9b32abc66defa64a71e6cf09e7685
Debora Gama <debora.gama@instruct.com.br>
2023-06-11 22:19:32 -0300

    doc(recipes): installing djanfo rest framework simple jwt

--- recipes/views/site.py
+++ recipes/views/site.py
@@ -11,22 +11,23 @@ from ..utils.pagination import make_pagination
 
 PER_PAGES = int(os.environ.get('PER_PAGE', 6))
 
+
 class RecipesListViewBase(ListView):
     model = Recipe
     context_object_name = 'recipes'
     paginate_by = None
-    ordering =  ['-id']
+    ordering = ['-id']
     template_name = 'recipes/pages/home.html'
 
-    def get_queryset(self, *args,**kwargs):
-        query_set = super().get_queryset(*args,**kwargs)
+    def get_queryset(self, *args, **kwargs):
+        query_set = super().get_queryset(*args, **kwargs)
         query_set = query_set.filter(
             is_published=True,
         )
         return query_set
 
-    def get_context_data(self, *args,**kwargs):
-        context = super().get_context_data(*args,**kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
 
         page_object, pagination_range = make_pagination(
             self.request,
@@ -56,11 +57,12 @@ class RecipesListViewHome(RecipesListViewBase):
         )
         return context
 
+
 class RecipesListCategory(RecipesListViewBase):
     template_name = 'recipes/pages/category.html'
 
-    def get_queryset(self, *args,**kwargs):
-        query_set = super().get_queryset(*args,**kwargs)
+    def get_queryset(self, *args, **kwargs):
+        query_set = super().get_queryset(*args, **kwargs)
 
         query_set.filter(
             category__id=self.kwargs.get('category_id')
@@ -75,7 +77,7 @@ class RecipesListCategory(RecipesListViewBase):
         ).order_by('-id'))
         title = f"{recipes[0].category.name} - Category | Recipes"
         page_object, pagination_range = make_pagination(
-        self.request, recipes, PER_PAGES)
+            self.request, recipes, PER_PAGES)
 
         context.update(
             {
@@ -86,11 +88,12 @@ class RecipesListCategory(RecipesListViewBase):
         )
         return context
 
+
 class RecipesListSearch(RecipesListViewBase):
     template_name = 'recipes/pages/search_page.html'
 
-    def get_queryset(self, *args,**kwargs):
-        query_set = super().get_queryset(*args,**kwargs)
+    def get_queryset(self, *args, **kwargs):
+        query_set = super().get_queryset(*args, **kwargs)
         search_term = self.request.GET.get('q', '').strip()
 
         if not search_term:
@@ -119,29 +122,23 @@ class RecipesListSearch(RecipesListViewBase):
         return context
 
 
-
-
-
-
-
-
-#These methods below are no longer being used. We refactored the home method using CBV,
+# These methods below are no longer being used. We refactored the home method using CBV,
 # but this code snippet is still here for teaching purposes only
-def home(request):
-    title = 'Home | Recipes'
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+# def home(request):
+#     title = 'Home | Recipes'
+#     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    page_object, pagination_range = make_pagination(
-        request, recipes, PER_PAGES)
+#     page_object, pagination_range = make_pagination(
+#         request, recipes, PER_PAGES)
 
-    return render(
-        request,
-        'recipes/pages/home.html',
-        context={'title': title, 'recipes': page_object,
-                 'pagination_range': pagination_range}
-    )
+#     return render(
+#         request,
+#         'recipes/pages/home.html',
+#         context={'title': title, 'recipes': page_object,
+#                  'pagination_range': pagination_range}
+#     )
 
-
+# TODO Refactor this code block using a view set module and change urls
 def recipe(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id, is_published=True)
     title = f"{recipe.title} | Recipes'"
@@ -152,40 +149,40 @@ def recipe(request, recipe_id):
     )
 
 
-def category(request, category_id):
-    recipes = get_list_or_404(Recipe.objects.filter(
-        category__id=category_id).order_by('-id'))
-    title = f"{recipes[0].category.name} - Category | Recipes"
+# def category(request, category_id):
+#     recipes = get_list_or_404(Recipe.objects.filter(
+#         category__id=category_id).order_by('-id'))
+#     title = f"{recipes[0].category.name} - Category | Recipes"
 
-    page_object, pagination_range = make_pagination(
-        request, recipes, PER_PAGES)
+#     page_object, pagination_range = make_pagination(
+#         request, recipes, PER_PAGES)
 
-    return render(
-        request,
-        'recipes/pages/category.html',
-        context={'title': title, 'recipes': page_object,
-                 'pagination_range': pagination_range}
-    )
+#     return render(
+#         request,
+#         'recipes/pages/category.html',
+#         context={'title': title, 'recipes': page_object,
+#                  'pagination_range': pagination_range}
+#     )
 
 
-def search(request):
-    # strip tira espaços da string
-    search_term = request.GET.get('q', '').strip()
-    title = f'Search for "{search_term}" | Recipes'
+# def search(request):
+#     # strip tira espaços da string
+#     search_term = request.GET.get('q', '').strip()
+#     title = f'Search for "{search_term}" | Recipes'
 
-    if not search_term:
-        raise Http404()
+#     if not search_term:
+#         raise Http404()
 
-    recipes = Recipe.objects.filter(
-        Q(Q(title__icontains=search_term) | Q(
-            description__icontains=search_term)), is_published=True,
-    ).order_by('-id')
+#     recipes = Recipe.objects.filter(
+#         Q(Q(title__icontains=search_term) | Q(
+#             description__icontains=search_term)), is_published=True,
+#     ).order_by('-id')
 
-    return render(
-        request,
-        'recipes/pages/search_page.html',
-        context={
-            'title': title,
-            'search_term': search_term,
-            'recipes': recipes},
-    )
+#     return render(
+#         request,
+#         'recipes/pages/search_page.html',
+#         context={
+#             'title': title,
+#             'search_term': search_term,
+#             'recipes': recipes},
+#     )
