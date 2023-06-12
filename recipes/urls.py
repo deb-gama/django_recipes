23f38fa25370296dee1430ec315f0dbe09f3864b
Debora Gama <debora.gama@instruct.com.br>
2023-06-11 21:13:24 -0300

    refactor(recipes): using viewset and deleting code duplications

--- recipes/urls.py
+++ recipes/urls.py
@@ -12,6 +12,16 @@ urlpatterns = [
         views.RecipesListCategory.as_view(), name="category"
     ),
     path("recipes/<int:recipe_id>/", views.recipe, name="recipe"),
-    path("recipes/api/v1/", views.RecipeAPIv1ViewSet.as_view({'get': 'list', 'post': 'create'}), name="recipe_api_v1"),
-    path("recipes/api/v1/<int:pk>", views.RecipeAPIv1ViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'}), name="recipe_api_v1_detail"),
+    path("recipes/api/v1/", views.RecipeAPIv1ViewSet.as_view(
+        {'get': 'list',
+         'post': 'create',
+         }
+    ), name="recipe_api_v1"),
+    path("recipes/api/v1/<int:pk>", views.RecipeAPIv1ViewSet.as_view(
+        {
+            'get': 'retrieve',
+            'patch': 'partial_update',
+            'delete': 'destroy',
+        }
+    ), name="recipe_api_v1_detail"),
 ]
--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,12 +1,14 @@
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.viewsets import ModelViewSet
 from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
 
@@ -20,20 +22,22 @@ class RecipeAPIv1ViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv1Pagination
 
-class RecipesAPIv1List(ListCreateAPIView):
-    """
-    View that contains the get and post methods and have the same url without a pk in it.
-    The 'get' method list recipes and the 'post' method create a new recipe.
-    """
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv1Pagination
 
-class RecipeAPIv1Detail(RetrieveUpdateDestroyAPIView):
-    """
-    View that contains the read, patch and delete methods and have the same url with a pk in it.
-    The 'get' method list recipes and the 'post' method create a new recipe.
-    """
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv1Pagination
+# This code block was replaced by ViewSet for avoid duplication
+# class RecipesAPIv1List(ListCreateAPIView):
+#     """
+#     View that contains the get and post methods and have the same url without a pk in it.
+#     The 'get' method list recipes and the 'post' method create a new recipe.
+#     """
+#     queryset = Recipe.objects.get_published()
+#     serializer_class = RecipeSerializer
+#     pagination_class = RecipeAPIv1Pagination
+
+# class RecipeAPIv1Detail(RetrieveUpdateDestroyAPIView):
+#     """
+#     View that contains the read, patch and delete methods and have the same url with a pk in it.
+#     The 'get' method list recipes and the 'post' method create a new recipe.
+#     """
+#     queryset = Recipe.objects.get_published()
+#     serializer_class = RecipeSerializer
+#     pagination_class = RecipeAPIv1Pagination
